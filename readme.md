7a7511fe745be62d321650246358e3d7f2ec8fb0
David Parsons <work@192-168-1-100.tpgi.com.au>
2017-07-06 21:11:00 +1000

    First commit

---
+++ readme.md
@@ -0,0 +1,99 @@
+# Buckets - Basketball Stats Tracking App
+
+This app is useful for tracking stats of teams and players that play in basketball leagues. The app contains a REST API backend and a React client
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+What things you need to install the software and how to install them
+Read: http://sgeek.org/create-restful-api-using-node-js-express-and-mysql/
+Note: If you download the Tutorial above, the server.js is missing some code. e.g. app.post
+
+```
+$ npm install
+ 
+$ npm install body-parser --save
+$ npm install express --save
+$ npm install mysql  --save
+
+```
+
+### Installing
+
+A step by step series of examples that tell you have to get a development env running
+
+Say what the step will be
+
+```
+Give the example
+```
+
+And repeat
+
+```
+until finished
+```
+
+End with an example of getting some data out of the system or using it for a little demo
+
+## Running the tests
+
+Explain how to run the automated tests for this system
+
+### Break down into end to end tests
+
+Explain what these tests test and why
+
+```
+Give an example
+```
+
+### And coding style tests
+
+Explain what these tests test and why
+
+```
+Give an example
+```
+
+## Deployment
+
+This app is deployed on a digitalocean VPS running Ubuntu
+
+## Built With
+
+* [React](https://facebook.github.io/react/) - The web framework used
+* [Bootstrap v3](getbootstrap.com) - Styling, grids, buttons
+* [NodeJS 7.10](https://nodejs.org) - back end
+* [ExpressJS 4](https://expressjs.com) - routes/server
+* [MySQL Community Edition](https://www.mysql.com/products/community/) - Database
+
+## Contributing
+
+Please read [CONTRIBUTING.md]() for details on our code of conduct, and the process for submitting pull requests to us.
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/your/project/tags). 
+
+## Authors
+
+* **BDavid Parsons** - *Initial work* - [DavidJParsons](https://github.com/davidjparsons)
+
+See also the list of [contributors](https://github.com/your/project/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* http://sgeek.org/create-restful-api-using-node-js-express-and-mysql/ for a great tutorial
+* deoxxa for linking me to http://blog.timoxley.com/post/47041269194/avoid-else-return-early 
+* Hat tip to anyone who's code was used
+* Inspiration
+* etc
+
---
+++ server.js
@@ -0,0 +1,188 @@
+// Basic Setup
+var http     = require('http'),
+	express  = require('express'),
+	mysql    = require('mysql')
+	parser   = require('body-parser');
+ 
+// Database Connection
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'admin_restapi',
+  password : '9aqeofTowL',
+  database : 'node_shop'
+});
+try {
+	connection.connect();
+	
+} catch(e) {
+	console.log('Database Connetion failed:' + e);
+}
+ 
+ 
+// Setup express
+var app = express();
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
+app.set('port', process.env.PORT || 3000);
+ 
+// Set default route
+app.get('/', function (req, res) {
+	res.send('<html><body><p>Welcome to sShop App</p></body></html>');
+});
+
+/*// Endpoint: http://127.0.0.1:5000/product/add
+app.post('/product/add', function (req,res) {
+	var response = [];
+
+	if (
+		typeof req.body.name !== 'undefined' && 
+		typeof req.body.price !== 'undefined' && 
+		typeof req.body.imageUrl !== 'undefined'
+	) {
+		var name = req.body.name, price = req.body.price, imageUrl = req.body.imageUrl;
+
+		connection.query('INSERT INTO nd_products (product_name, product_price, product_image) VALUES (?, ?, ?)', 
+			[name, price, imageUrl], 
+			function(err, result) {
+		  		if (!err){
+
+					if (result.affectedRows != 0) {
+						response.push({'result' : 'success'});
+					} else {
+						response.push({'msg' : 'No Result Found'});
+					}
+
+					res.setHeader('Content-Type', 'application/json');
+			    	res.status(200).send(JSON.stringify(response));
+		  		} else {
+				    res.status(400).send(err);
+			  	}
+			});
+
+	} else {
+		response.push({'result' : 'error', 'msg' : 'Please fill required details'});
+		res.setHeader('Content-Type', 'application/json');
+    	res.status(200).send(JSON.stringify(response));
+	}
+});*/
+
+
+//refactored if (err) as per deoxxa's suggestion
+app.post('/product/add', function (req,res) {
+	var response = [];
+
+	if (
+		typeof req.body.name !== 'undefined' && 
+		typeof req.body.price !== 'undefined' && 
+		typeof req.body.imageUrl !== 'undefined'
+	) {
+		var name = req.body.name, price = req.body.price, imageUrl = req.body.imageUrl;
+
+		connection.query('INSERT INTO nd_products (product_name, product_price, product_image) VALUES (?, ?, ?)', 
+			[name, price, imageUrl], 
+			function(err, result) {
+		  		if (err) { res.status(400).send(err); return; }
+                  {
+					if (result.affectedRows != 0) {
+						response.push({'result' : 'success'});
+					} else {
+						response.push({'msg' : 'No Result Found'});
+					}
+
+					res.setHeader('Content-Type', 'application/json');
+			    	res.status(200).send(JSON.stringify(response));
+		  		} 
+			});
+
+	} else {
+		response.push({'result' : 'error', 'msg' : 'Please fill required details'});
+		res.setHeader('Content-Type', 'application/json');
+    	res.status(200).send(JSON.stringify(response));
+	}
+});
+
+// Endpoint: http://127.0.0.1:5000/product/{:product id}
+app.get('/product/:id', function (req,res) {
+	var id = req.params.id;
+ 
+	connection.query('SELECT * from nd_products where id = ?', [id], function(err, rows, fields) {
+  		if (!err){
+  			var response = [];
+ 
+			if (rows.length != 0) {
+				response.push({'result' : 'success', 'data' : rows});
+			} else {
+				response.push({'result' : 'error', 'msg' : 'No Results Found'});
+			}
+ 
+			res.setHeader('Content-Type', 'application/json');
+	    	res.status(200).send(JSON.stringify(response));
+  		} else {
+		    res.status(400).send(err);
+	  	}
+	});
+});
+
+// Endpoint: http://127.0.0.1:5000/product/edit/{:product id}
+app.post('/product/edit/:id', function (req,res) {
+	var id = req.params.id, response = [];
+ 
+	if (
+		typeof req.body.name !== 'undefined' && 
+		typeof req.body.price !== 'undefined' && 
+		typeof req.body.imageUrl !== 'undefined'
+	) {
+		var name = req.body.name, price = req.body.price, imageUrl = req.body.imageUrl;
+ 
+		connection.query('UPDATE nd_products SET product_name = ?, product_price = ?, product_image = ? WHERE id = ?', 
+			[name, price, imageUrl, id], 
+			function(err, result) {
+		  		if (!err){
+ 
+					if (result.affectedRows != 0) {
+						response.push({'result' : 'success'});
+					} else {
+						response.push({'msg' : 'No Result Found'});
+					}
+ 
+					res.setHeader('Content-Type', 'application/json');
+			    	res.status(200).send(JSON.stringify(response));
+		  		} else {
+				    res.status(400).send(err);
+			  	}
+			});
+ 
+	} else {
+		response.push({'result' : 'error', 'msg' : 'Please fill required information'});
+		res.setHeader('Content-Type', 'application/json');
+    	res.send(200, JSON.stringify(response));
+	}
+});
+
+// Endpoint: http://127.0.0.1:5000/product/delete/{:product id}
+app.delete('/product/delete/:id', function (req,res) {
+	var id = req.params.id;
+ 
+	connection.query('DELETE FROM nd_products WHERE id = ?', [id], function(err, result) {
+  		if (!err){
+  			var response = [];
+ 
+			if (result.affectedRows != 0) {
+				response.push({'result' : 'success'});
+			} else {
+				response.push({'msg' : 'No Result Found'});
+			}
+ 
+			res.setHeader('Content-Type', 'application/json');
+	    	res.status(200).send(JSON.stringify(response));
+  		} else {
+		    res.status(400).send(err);
+	  	}
+	});
+});
+ 
+// Create server
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('Server listening on port ' + app.get('port'));
+});
+ 
